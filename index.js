06c8a3033d594f08676b46b3968e26ac77984dbf
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-18 00:19:25 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const testVar = "hi"
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
